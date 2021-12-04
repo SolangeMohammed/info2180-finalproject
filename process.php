9d6351b77053fdbb2f4d54fdde3597a1815018b6
Solange Mohammed <solangemohammed123@gmail.com>
2021-12-04 19:00:02 -0400

    full process

--- process.php
+++ process.php
@@ -1,5 +1,6 @@
 <?php
-
+session_id(); 
+session_start(); 
 $host = 'localhost';
 $username = 'admin@project2.com';
 $password = 'password123';
@@ -10,28 +11,48 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $stmt = $conn->query("SELECT * FROM issues");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+?> 
+<?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
   date_default_timezone_set('Jamaica');
-  $name = trim(filter_var($_POST['assigned'], FILTER_SANITIZE_STRING));
+  $assigned_to = trim(filter_var($_POST['assigned'], FILTER_SANITIZE_STRING));
   $title = trim(filter_var($_POST['title'], FILTER_SANITIZE_STRING));
   $dr = trim(filter_var($_POST['description'], FILTER_SANITIZE_STRING));
   $ty = trim(filter_var($_POST['type'], FILTER_SANITIZE_STRING)) ;
   $priority = trim(filter_var($_POST['priority'], FILTER_SANITIZE_STRING));
   $stat = "Open"; 
+  
+
+  // $fullname = explode(" ",$assigned_name);
+  // $fname = $fullname[0];
+  // $lname = $fullname[1]; 
+  // $stmt1 = $conn->query("SELECT id FROM users WHERE firstname='$fname' AND lastname='$lname'");
+  // $stmt1->execute();
+  
+
+  $created_by = $_SESSION['id']; 
+  $stmt1 = $conn->query("SELECT firstname, lastname FROM users WHERE id= '$created_by'");
+  $result = $stmt1->fetch(PDO::FETCH_ASSOC); 
+  $fname = $result['firstname']; 
+  $lname = $result['lastname']; 
+  $createdby_nl = $fname." " . $lname; 
+
+
+ 
   $created =  date("Y-m-d H:i:s");   
   $updated = date("Y-m-d H:i:s");
-  $sql = "INSERT INTO issues (`title`, `description`, `type`,`priority`,`status`, `assigned_to`,`created`, `updated`) 
-  VALUES ('$title', '$dr', '$ty', '$priority', '$stat','$name', '$created', '$updated')";
+  $sql = "INSERT INTO issues (`title`, `description`, `type`,`priority`,`status`, `assigned_to`,`created_by`,`created`, `updated`) 
+  VALUES ('$title', '$dr', '$ty', '$priority', '$stat','$assigned_to', '$createdby_nl', '$created', '$updated')";
   $conn->exec($sql); 
 
   echo "New records created successfully";
+  echo $createdby_nl;
   echo "The title: <strong>" . $_POST['title'] . "</strong> Description: <strong>" . $_POST['description'] . "</strong>.";
   echo "type:" .$_POST['type'] . "priority: <strong>" .$_POST['priority'] . "</strong>."  ; 
-  echo $status; 
   echo $created;
-  echo $name;  
+  echo $created_by; 
+  echo $assigned_to;  
   
   
   }else{
@@ -39,15 +60,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo "failed"; 
 
   }
-  // if (!isset($_POST['title']) || !isset($_POST['description']) ||
-  // empty($_POST['title']) || empty($_POST['description'])) {
-  //   echo "What?!?!? You don't have a name or ID Number?";
-  // } else {
-  //   echo "Your name is: <strong>" . $_POST['title'] . "</strong> and your ID Number is: <strong>" . $_POST['description'] . "</strong>.";
-  //   $sql = "INSERT INTO issues (`title`, `description`)
-  //   VALUES ('$title', '$description')";
-  //   mysql_query($conn, $sql);
-    
-  // }
-// }
+ 
 ?>
